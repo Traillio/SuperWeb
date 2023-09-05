40563e1817184b431e638e4285ca80a9700883e6
Traill Lyall <holycrapeh@hotmail.com>
2023-09-05 14:51:10 -0700

    first commit

---
+++ Pages/Futuure.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperWeb.Models;
+
+namespace SuperWeb.Pages
+{
+    public class FutureValueModel : PageModel
+    {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["FV"] = 0;
+        }
+
+        public void OnPost(FutureValue model)
+        {
+            ViewData["FV"] = model.CalculateFutureValue();
+        }
+    }
+}
